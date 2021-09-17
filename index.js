d3caf6d3b3e4e0cf5dfe40a991678195e8836285
ismakova <ismakova.dayana@mail.ru>
2021-09-17 21:33:01 +0500

    ssl

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
+const enforce = require('express-sslify');
 
 const sequelize = require('./db');
 const router = require('./routes/index');
@@ -20,13 +21,23 @@ app.use(fileUpload({}));
 app.use('/api', router);
 
 app.use(errorHandler);
-
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 const start = async() =>{
     try {
        await sequelize.authenticate();
        await sequelize.sync();
         app.listen(PORT, ()=> console.log(`App has been started on port ${PORT}`));
+
+        // if(process.env.NODE_ENV === 'production') {
+        //     app.use((req, res, next) => {
+        //         if (req.header('x-forwarded-proto') !== 'https')
+        //             res.redirect(`https://${req.header('host')}${req.url}`)
+        //         else
+        //             next()
+        //     })
+        // }
+
     }   catch (e){
         console.log('Server Error', e.message);
         process.exit(1);
